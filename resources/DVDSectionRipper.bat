26c88e7c719b2206b010b6035d394d751669f5fa
ampp33 <ampp33@gmail.com>
2017-03-31 00:10:19 -0400

    added build script, bat, and util updates

---
+++ resources/DVDSectionRipper.bat
@@ -0,0 +1,3 @@
+start javaw -DworkingDir="%~dp0\" ^
+		-cp "%~dp0lib\*;%~dp0DVDSectionRipper.jar" ^
+		org.malibu.dvd.sectionripper.main.DVDSectionRipper
--- src/org/malibu/dvd/sectionripper/utilities/Util.java
+++ src/org/malibu/dvd/sectionripper/utilities/Util.java
@@ -215,16 +215,17 @@ public class Util {
 	
 	private static String getJarDirectory() {
 		// get directory .jar file is running from (using substring() to remove leading slash)
-		String workingDir = Util.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-		File file = new File(workingDir);
-		workingDir = file.getAbsolutePath();
-		if(workingDir.startsWith("\\")) {
-			workingDir = workingDir.substring(1);
-		}
-		if(workingDir.endsWith(".")) {
-			workingDir = workingDir.substring(0, workingDir.length() - 2);
-		}
-		return workingDir;
+//		String workingDir = Util.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+//		File file = new File(workingDir);
+//		workingDir = file.getAbsolutePath();
+//		if(workingDir.startsWith("\\")) {
+//			workingDir = workingDir.substring(1);
+//		}
+//		if(workingDir.endsWith(".")) {
+//			workingDir = workingDir.substring(0, workingDir.length() - 2);
+//		}
+//		return workingDir;
+		return System.getProperty("workingDir");
 	}
 	
 	/**
